cc833b243bd25f3a6a95aebf00b7018755451916
Philgyam <philemongyamfi9@gmail.com>
2025-03-04 17:22:27 +0000

    chore(view):adding time tracker to dashboard

--- src/components/ui/Layout/View.tsx
+++ src/components/ui/Layout/View.tsx
@@ -1,5 +1,7 @@
 import Admin from "@/screens/Admin";
 import Dashboard from "@/screens/Dashboard";
+import Timetracker from "@/screens/Timetracker";
+
 import { Route, Routes } from "react-router-dom";
 
 export const View = () => {
@@ -7,6 +9,7 @@ export const View = () => {
     <Routes>
       <Route path="/" element={<Dashboard />} />
       <Route path="/admin" element={<Admin />} />
+      <Route path="/timetracker" element={<Timetracker/>} />
     </Routes>
   );
 };
