b4a7de7c6e485a1c51e523383e0355ff1dda3285
Philgyam <philemongyamfi9@gmail.com>
2025-03-04 17:16:07 +0000

    chore(confirmDelete): Project delection functionality

---
+++ src/components/ProjectComponents/Modals/ConfirmDeleteModal.tsx
@@ -0,0 +1,30 @@
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
+import { ConfirmModalProps } from '@/types';
+
+function ConfirmModal({ isOpen, onConfirm, onCancel, message }: ConfirmModalProps) {
+    return (
+        <Dialog open={isOpen} onOpenChange={() => {}} modal>
+            <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                    <DialogTitle>Confirm Deletion</DialogTitle>
+                    <DialogDescription>
+                        <span>Are you sure you want to delete </span>
+                        <br />
+                        <span className=' text-black'>{message} </span>
+                    </DialogDescription>
+                </DialogHeader>
+                <DialogFooter>
+                    <Button variant="outline" onClick={onCancel}>
+                        Cancel
+                    </Button>
+                    <Button onClick={onConfirm} className="ml-2 bg-red-500 hover:bg-red-600 text-white">
+                        Delete
+                    </Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    );
+}
+
+export default ConfirmModal;
