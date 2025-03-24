d4ada2cdbc50ee6347eec98b91645570e96b82e9
Morvic-Scp <gabbyawuah011@gmail.com>
2025-03-24 16:04:25 +0000

    chore(app):  Adding user teams route to api

--- src/API/EndPoints.ts
+++ src/API/EndPoints.ts
@@ -1,6 +1,7 @@
 export const API_ENDPOINTS = {
     LOGIN_API:"/token",
     LOGOUT:"/logout",
+    ALL_USER_TEAMS:"/user-teams",
     TOKEN_REFRESH:"/refresh-token",
     REGISTER:"/register",
     ALL_PROJECTS:"/user-projects",
