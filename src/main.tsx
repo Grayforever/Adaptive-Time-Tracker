19678b8b99281bc9c82bcd959766c60b9cf0a978
philgyam <philemongyamfi9@gmail.com>
2025-03-24 08:13:38 +0000

    chore(app):PWA fixes

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById("root")!).render(
 
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("sw.js")
+    .register("/sw.js")
     .then((registration) => {
       console.log("Service Worker registered with scope:", registration.scope);
     })
