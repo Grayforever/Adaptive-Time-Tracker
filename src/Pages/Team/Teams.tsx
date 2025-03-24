21e335632d171769cfcf34beb82ee5cda8920fef
Morvic-Scp <gabbyawuah011@gmail.com>
2025-03-24 09:59:50 +0000

    chore(teams):Creating a Component directory for Teams UI.

---
+++ src/Pages/Team/Teams.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Teams = () => {
+  return (
+    <div className="p-5">
+      <div className="px-[2vw] py-[3vh] flex">
+        <h3 className="text-gray-400 text-1xl md:text-3xl">Teams</h3>
+      </div>
+      
+      <div className="bg-white w-full px-[2vw] py-[3vw] rounded-3xl">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Teams
